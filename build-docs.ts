7562c9f0e06145143a58b6deb974e378c7e22081
Nicolás Serna <nicolito128@hotmail.com>
2022-07-16 20:15:55 -0300

    Update build-docs: refact all

--- build-docs.ts
+++ build-docs.ts
@@ -3,20 +3,15 @@
  *         | deno run --allow-all build-docs.ts
 */
 
-// Node imports
-import { ensureFile } from 'https://deno.land/std@0.148.0/fs/mod.ts';
-import { join } from "https://deno.land/std@0.148.0/path/mod.ts";
+// Imports
+import { doc } from 'https://deno.land/x/deno_doc@0.38.0/mod.ts';
 
-// Path with the docs.json file generated with `deno doc --json mod.ts > docs.json`
-const docsPath = join('./docs.json');
-const mainBranchURL = 'https://github.com/oasisjs/biscuit/tree/main';
-
-/** JSON Start */
-interface DocumentationObject {
+interface DocNodeProcessed {
     kind: string;
     name: string;
-    location: Location;
-    declarationKind: string;
+    url: string;
+    rawURL: string;
+    expression: string;
 }
 
 interface Location {
@@ -25,123 +20,99 @@ interface Location {
     col: number;
 }
 
-interface VariableObject extends DocumentationObject {
-    kind: 'variable';
-    variableDef: VariableDef;
-}
-
-interface VariableDef {
-    tsType: TsType;
-    kind: string;
-}
-
-interface TsType {
-    repr: string;
-    kind: string;
-    keyword?: string;
-    typeRef?: TypeRef;
-}
-
-interface TypeRef {
-    typeName?: string;
-    typeParams: TsType[] | null;
-}
-/** JSON end */
-
-// Interface to represent a processed DocumentationObject 
-interface DocumentationComponent {
-    // Parsed expression. Ex: class Foo(bar: string, baz: ComplexType)
-    expression: string;
-    // variable, enum, class, interface, etc.
-    kind: string;
-    // The name of the type/class. Ex: Foo
-    name: string;
-    // Main branch url.
-    url: string;
-}
+const branchURL = 'https://github.com/oasisjs/biscuit/tree/main';
+const rawBranchURL = 'https://raw.githubusercontent.com/oasisjs/biscuit/main';
 
 /**
  * Returns a new javascript object with the docs.json file.
 */
-function getDocs(): DocumentationObject[] {
-    if (!ensureFile(docsPath)) {
-        throw new Error('The docs.json file does not exist. Use: deno doc --json mod.ts > docs.json');
-    }
-
-    const res = Deno.readFileSync(docsPath)
-    const decoder = new TextDecoder('utf-8');
-    return JSON.parse(decoder.decode(res).trim().replaceAll(/[^A-Za-z0-9\"\:\{\}\[\]\.\,\/\\\_\-]/g, ''));
+function getDocs() {
+    return doc(rawBranchURL + '/mod.ts');
 }
 
 /**
  * Get the file location at the main branch of the repository.
 */
-function getLocationURL(location: Location): string {
+function getLocationURL(location: Location, options: 'raw' | 'main' = 'main'): string {
+    let url: string;
+    if (options === 'raw') {
+        url = rawBranchURL
+    } else {
+        url = branchURL
+    }
+
     const filename = location.filename;
     const start = filename.indexOf('/packages/');
     const componentPath = filename.slice(start, filename.length);
     const lineAndCol = `#L${location.line}:${location.col}`;
 
-    return mainBranchURL + componentPath + lineAndCol;
+    return url + componentPath + lineAndCol;
 }
 
-function loadComponent(docElement: DocumentationObject): DocumentationComponent | null {
-    if (
-        docElement.declarationKind != 'export' ||
-        docElement.name == "default" ||
-        docElement.kind == 'import'
-    ) {
-        return null;
-    }
-
-    const url = getLocationURL(docElement.location);
-    //const exp = parseDocumentationToExpression(docElement);
-    return {
-        expression: "",
-        kind: docElement.kind,
-        name: docElement.name,
-        url: url
-    };
+// TODO: finish this
+async function makeDocumentation() {
+    const docs = await loadDocs();
+    console.log(docs);
 }
 
-/**
- * Parse the doc element to get the expression.
-*/
-function parseDocumentationToExpression(docElement: DocumentationObject): string {
-    let parsed: string = '';
+async function loadDocs(): Promise<DocNodeProcessed[]> {
+    const docs = await getDocs();
+    const arr: DocNodeProcessed[] = [];
 
-    if (docElement.kind === 'variable') {
-        const varobj = docElement as VariableObject;
-        // TODO: parse
-    }
-
-    return parsed;
-}
-
-/**
- * 
- * @param {any[]} docs - The docs.json file
- * @returns {{expression: string, kind: string, name: string, url: string}[]}
-*/
-function loadAllComponents(docs: DocumentationObject[]): DocumentationComponent[] {
-    const arr: DocumentationComponent[] = [];
-
-    docs.forEach(element => {
-        const comp = loadComponent(element);
-        
-        if (comp) {
-            arr.push(comp);
+    for (const node of docs) {
+        if (node.declarationKind !== 'export' || node.name === 'default' || node.kind === 'import') {
+            continue;
         }
-    })
+
+        let element = '';
+
+        switch (node.kind) {
+            case 'variable':
+                if (node.variableDef?.tsType === null) {
+                    continue;
+                }
+
+                if (node?.variableDef) {
+                    if (node?.variableDef?.kind) {
+                        element += `${node?.variableDef?.kind} ${node.name}`;
+                    }
+                    
+                    if (node?.variableDef.tsType) {
+                        if (node?.variableDef?.tsType?.kind === 'literal') {
+                            element += `: ${node?.variableDef?.tsType?.literal?.kind}`;
+                        } else if (node?.variableDef?.tsType?.kind === 'typeRef') {
+                            element += `: ${node?.variableDef?.tsType?.typeRef?.typeName}`;
+                        } else {
+                            element += `: ${node?.variableDef?.tsType?.repr}`;
+                        }
+                    }
+                }
+
+                arr.push({
+                    kind: 'variable',
+                    name: node.name,
+                    url: getLocationURL(node.location),
+                    rawURL: getLocationURL(node.location, 'raw'), 
+                    expression: element,
+                });
+            break;
+            case 'enum':
+                element = `enum ${node.name}`;
+
+                arr.push({
+                    kind: 'enum',
+                    name: node.name,
+                    url: getLocationURL(node.location),
+                    rawURL: getLocationURL(node.location, 'raw'), 
+                    expression: element,
+                });
+            break;
+
+            // TODO: classes, functions, interfaces.
+        }
+    }
 
     return arr;
 }
 
-try {
-    const docs = getDocs();
-    const load = loadAllComponents(docs);
-
-    console.log(load)
-} catch(err) {
-    throw new Error(err);
-}
+makeDocumentation();
